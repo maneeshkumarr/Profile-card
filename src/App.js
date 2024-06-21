ef29247b475b6b2f12bd31f0cba3be202b515ced
Maneesh Kumar <maneeshpersonal638.com>
2024-06-21 17:00:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import ProfileCard from './ProfileCard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Profile Card</h1>
+      </header>
+      <main>
+        <ProfileCard />
+      </main>
+    </div>
+  );
+}
+
+export default App;
